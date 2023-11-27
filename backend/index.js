63800243554a64565c855ee25b291b4e3740d593
Arun <arunsah98078@gmail.com>
2023-11-27 18:24:06 +0530

    upto_video46

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,15 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+//available routes_________________
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(express.json())
+
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
